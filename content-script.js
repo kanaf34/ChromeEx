33f961bb2bafd73b3b2657ed65b77e0078f9b0ab
kanaf34 <kanaf34@gmail.com>
2015-10-27 01:37:43 +0900

    check only debug

--- content-script.js
+++ content-script.js
@@ -1,13 +1,30 @@
-var injectScript;
+// var injectScript;
 
-injectScript = function(file, node) {
-	var s, th;
-	th = document.getElementsByTagName(node)[0];
-	s = document.createElement('script');
-	s.setAttribute('type', 'text/javascript');
-	s.setAttribute('src', file);
+// injectScript = function(file, node) {
+// 	var s, th;
+// 	th = document.getElementsByTagName(node)[0];
+// 	s = document.createElement('script');
+// 	s.setAttribute('type', 'text/javascript');
+// 	s.setAttribute('src', file);
 
-	return th.appendChild(s);
-};
+// 	return th.appendChild(s);
+// };
 
-injectScript(chrome.extension.getURL('/embeded-script.js'), 'body');
+// injectScript(chrome.extension.getURL('/embeded-script.js'), 'body');
+
+// 화면 페이지에 직접적으로 주입이 가능한 자바스크립트
+
+(function() {
+	alert(1)
+
+	chrome.runtime.onMessage.addListener(function(message) {
+		if (message.type === 'onActivated') {
+			return chrome.runtime.sendMessage({
+				type:'setBadgeText',
+				value:'10'
+			});
+		}
+
+	});
+
+}).call(this);
--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,7 @@
-$(function() {
-	chrome.tabs.getSelected(null, function(tab) {
-		$('#title').text(tab.title);
-		$('#url').text(tab.url);
-	})
-})
+// $(function() {
+// 	// tabs 에 있는 메소드
+// 	chrome.tabs.getSelected(null, function(tab) {
+// 		$('#title').text(tab.title);
+// 		$('#url').text(tab.url);
+// 	})
+// })
