b84b365f03ddfedb088a530ef9f8701fef530872
kanaf34 <kanaf34@gmail.com>
2015-10-29 00:40:40 +0900

    팝업과 메인페이지간의 데이터이동방법확인

--- content-script.js
+++ content-script.js
@@ -15,16 +15,27 @@
 // 화면 페이지에 직접적으로 주입이 가능한 자바스크립트
 
 (function() {
-	alert(1)
+	// chrome.runtime.onMessage.addListener(function(message) {
+	// 	alert(1)
+	// 	if (message.type === 'onActivated') {
 
-	chrome.runtime.onMessage.addListener(function(message) {
-		if (message.type === 'onActivated') {
-			return chrome.runtime.sendMessage({
-				type:'setBadgeText',
-				value:'10'
-			});
-		}
 
-	});
+
+	// 		return chrome.runtime.sendMessage({
+	// 			type:'setBadgeText',
+	// 			value:'10'
+	// 		});
+	// 	}
+
+	// });
+
+	chrome.runtime.onMessage.addListener(
+	  function(request, sender, sendResponse) {
+	    console.log(sender.tab ?
+	                "from a content script:" + sender.tab.url :
+	                "from the extension");
+	    if (request.greeting == "hello")
+	      sendResponse({farewell: "goodbye"});
+	  });
 
 }).call(this);
--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,14 @@
-// $(function() {
-// 	// tabs 에 있는 메소드
-// 	chrome.tabs.getSelected(null, function(tab) {
-// 		$('#title').text(tab.title);
-// 		$('#url').text(tab.url);
-// 	})
-// })
+$(function() {
+
+	//alert(2)
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+	    alert(response.farewell);
+	  });
+	});
+	// tabs 에 있는 메소드
+	// chrome.tabs.getSelected(null, function(tab) {
+	// 	$('#title').text(tab.title);
+	// 	$('#url').text(tab.url);
+	// })
+})
