55c492da6fda9dada9c0f6a68d06c75e0acd8d96
kanaf34 <kanaf34@gmail.com>
2015-10-30 01:42:21 +0900

    아마존에서 데이터를 추출, 메시징을 통해 팝업창에 전달

--- content-script.js
+++ content-script.js
@@ -18,24 +18,47 @@
 	// chrome.runtime.onMessage.addListener(function(message) {
 	// 	alert(1)
 	// 	if (message.type === 'onActivated') {
-
-
-
 	// 		return chrome.runtime.sendMessage({
 	// 			type:'setBadgeText',
 	// 			value:'10'
 	// 		});
 	// 	}
-
 	// });
 
+	// 메세지가 전달되어 온 경우
 	chrome.runtime.onMessage.addListener(
 	  function(request, sender, sendResponse) {
-	    console.log(sender.tab ?
-	                "from a content script:" + sender.tab.url :
-	                "from the extension");
-	    if (request.greeting == "hello")
-	      sendResponse({farewell: "goodbye"});
+	    // console.log(sender.tab ?
+	    //             "from a content script:" + sender.tab.url :
+	    //             "from the extension");
+
+		// 데이터 가져올 떄 amazon.jp 에서 : 그냥 책일 경우, 킨들책일 경우 구분해서 read
+
+		// 그냥 책일 경우
+
+		// 2015.10.30
+		// amazon.jp 책 화면의 데이터를 추출, 메시지로 전달하기
+		// var authorTag = document.getElementById('booksTitle').getElementsByClassName('author')[0];
+		// var authorNm = authorTag.getElementsByClassName('contributorNameID')[0].textContent;
+
+		var title = document.getElementById('productTitle').textContent;
+		var imgUrl = document.getElementById('img-canvas').firstElementChild.getAttribute('src');
+		var priceTag = document.getElementById('a-autoid-2-announce');
+		var priceVal = (priceTag.getElementsByClassName('a-color-price')[0]).textContent.trim();
+
+// {
+// 	      	author : authorNm,
+// 	      	title  : title,
+// 	      	price  : priceVal,
+// 	      	img    : imgUrl,
+// 	      	owner  : 'Amazon.jp'
+// 	      }
+
+
+	    if (request.gretting == "hello")
+	      sendResponse({
+	      	title : title
+	      });
 	  });
 
 }).call(this);
--- js/popup.js
+++ js/popup.js
@@ -4,8 +4,12 @@ $(function() {
 
 	// 팝업과 메인 페이지간의 데이터 전달은 Message를 통해서
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-	    alert(response.farewell);
+	  chrome.tabs.sendMessage(tabs[0].id, {gretting: "hello"}, function(response) {
+	    alert(response.title);
+		//$('#title').text(response.title);
+		$('#title').text(response.title);
+
+	    
 	  });
 	});
 	// tabs 에 있는 메소드
