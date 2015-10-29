4965461a9b45e8797cee8b86b6b95a4d113e42bd
kanaf34 <kanaf34@gmail.com>
2015-10-29 23:08:09 +0900

    backup

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,8 @@
 $(function() {
 
-	//alert(2)
+	// alert(2) : 
+
+	// 팝업과 메인 페이지간의 데이터 전달은 Message를 통해서
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
 	    alert(response.farewell);
