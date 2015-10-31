30c2778da8800e8b088b954ff5fd2dba86f3c30f
kanaf34 <kanaf34@gmail.com>
2015-11-01 01:02:05 +0900

    Amazon.jp 에서 책 데이터(제목,작가,값,이미지등)을 불러와서 팝업으로 띄워서 보여줌

--- content-script.js
+++ content-script.js
@@ -14,6 +14,74 @@
 
 // 화면 페이지에 직접적으로 주입이 가능한 자바스크립트
 
+
+// var 로 함수 정의해두는 것과 그냥 함수 적어놓는 것의 차이는 무엇?
+function funcAmazonJP() {
+	// 데이터 가져올 떄 amazon.jp 에서 : 그냥 책일 경우, 킨들책일 경우 구분해서 read
+	// bookTitle div element
+	var elBook = document.getElementById("booksTitle") || false; 
+	if (elBook)	// PaperBook 일 경우 Pasing
+		return funcParsingAmazonBook(elBook);
+
+	// kindle 일 경우 파싱
+	return funcParsingAmazonKindle();
+}
+
+function funcParsingAmazonKindle() {
+
+	var elTemp = document.getElementsByClassName('parseasinTitle');
+
+	var elAuthor = elTemp ? elTemp[0].nextElementSibling : '';	// 작가는 대표 한사람만.
+	var elTitle  = document.getElementById('btAsinTitle');
+	var elPrice  = document.getElementsByName('displayedPrice'); 
+	var elImg    = document.getElementById('main-image');
+	//var elASIN   = document.getElementById("ASIN.0");  // Amazon 고유번호, kindle은 ISBN과 다름
+
+	var elPriceCode  = document.getElementsByName('displayedPriceCurrencyCode'); 
+
+	return {
+	    title  : (elTitle  ? elTitle.innerText : '')
+	  , author : (elAuthor ? elAuthor.innerText  : '')
+	  , imgUrl : (elImg ? elImg.getAttribute('src') : '')
+	  , price  : (elPrice ? elPrice[0].value : '')
+	  , ISBN   : ''
+	  // 하단은 직접 작성
+	  , owner  : 'Amazon.jp'
+	  , isKindle : true
+	  , isOrigin : true
+	  , currencyCode : (elPriceCode ? elPriceCode[0].value : '')
+	}
+}
+
+
+function funcParsingAmazonBook(elBook) {
+
+	// 2015.10.30
+	// amazon.jp 책 화면의 데이터를 추출, 메시지로 전달하기
+	var elAuthor = elBook.getElementsByClassName('a-link-normal');	// 작가는 대표 한사람만.
+	var elTitle  = document.getElementById('productTitle');
+	var elPrice  = document.getElementsByClassName('offer-price'); 
+	var elImg    = document.getElementById('img-canvas').firstElementChild;
+	var elASIN   = document.getElementById("ASIN");  // Amazon 고유번호, 책일 경우 ISBN 과 동일
+
+	return {
+	    title  : (elTitle  ? elTitle.innerText : '')
+	  , author : (elAuthor ? elAuthor[0].innerText  : '')
+	  , imgUrl : (elImg ? elImg.getAttribute('src') : '')
+	  , price  : (elPrice ? (elPrice[0].innerText).substring(2) : '')
+	  , ISBN   : (elASIN  ? elASIN.value : '')
+	  // 하단은 직접 작성
+	  , owner  : 'Amazon.jp'
+	  , isKindle : false
+	  , isOrigin : true
+	  , currencyCode : 'JPY'
+	}
+
+	// 팝업창에 라디오버튼으로 킨들/일반책 구분
+	// 타이틀, 권수, 구입처, 구입일, 가격, 태그
+	// 가격은..음 일단 보이는 그대로의 데이터로 작성
+}
+
 (function() {
 	// chrome.runtime.onMessage.addListener(function(message) {
 	// 	alert(1)
@@ -27,38 +95,19 @@
 
 	// 메세지가 전달되어 온 경우
 	chrome.runtime.onMessage.addListener(
-	  function(request, sender, sendResponse) {
+	  function(message, sender, sendResponse) {
 	    // console.log(sender.tab ?
 	    //             "from a content script:" + sender.tab.url :
 	    //             "from the extension");
+	    if (message.type == 'onActivated') {
+	    	var url = document.URL; 
+	    	var rsltObj;
+	    	debugger;
+	    	if (url.search('amazon') > 0) 
+	    	   	rsltObj = funcAmazonJP();
 
-		// 데이터 가져올 떄 amazon.jp 에서 : 그냥 책일 경우, 킨들책일 경우 구분해서 read
-
-		// 그냥 책일 경우
-
-		// 2015.10.30
-		// amazon.jp 책 화면의 데이터를 추출, 메시지로 전달하기
-		// var authorTag = document.getElementById('booksTitle').getElementsByClassName('author')[0];
-		// var authorNm = authorTag.getElementsByClassName('contributorNameID')[0].textContent;
-
-		var title = document.getElementById('productTitle').textContent;
-		var imgUrl = document.getElementById('img-canvas').firstElementChild.getAttribute('src');
-		var priceTag = document.getElementById('a-autoid-2-announce');
-		var priceVal = (priceTag.getElementsByClassName('a-color-price')[0]).textContent.trim();
-
-// {
-// 	      	author : authorNm,
-// 	      	title  : title,
-// 	      	price  : priceVal,
-// 	      	img    : imgUrl,
-// 	      	owner  : 'Amazon.jp'
-// 	      }
-
-
-	    if (request.gretting == "hello")
-	      sendResponse({
-	      	title : title
-	      });
+	      	sendResponse(rsltObj);
+	    }
 	  });
 
 }).call(this);
--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,19 @@
 $(function() {
-
-	// alert(2) : 
-
 	// 팝업과 메인 페이지간의 데이터 전달은 Message를 통해서
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	  chrome.tabs.sendMessage(tabs[0].id, {gretting: "hello"}, function(response) {
-	    alert(response.title);
-		//$('#title').text(response.title);
-		$('#title').text(response.title);
+	  chrome.tabs.sendMessage(tabs[0].id, {type : 'onActivated'}, function(response) {
+	    
+		$('#rsltObj').val(response);
+		
+		$('#owner').val(response.owner);
+		$('#title').val(response.title);
+		$('#author').val(response.author);
+		$('#price').val(response.price);
+		$('#image').attr("src", response.imgUrl);
+		response.isOrigin ? $("#originY").attr("checked", "checked") 
+		                  : $("#originN").attr("checked", "checked"); 
+		response.isKindle ? $("#kindleY").attr("checked", "checked") 
+		                  : $("#kindleN").attr("checked", "checked"); 
 
 	    
 	  });
